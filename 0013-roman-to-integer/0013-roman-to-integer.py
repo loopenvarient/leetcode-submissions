6f04479b644d0327958215952a77096d01481407
Armeen Asim <armeen.aasim@gmail.com>
2026-08-21 22:48:16 +0500

    Time: 4 ms (64.01%), Space: 19.1 MB (98.48%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,22 @@
+class Solution:
+    def romanToInt(self, s):
+        val = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        total = 0
+
+        for i in range(len(s) - 1):
+            if val[s[i]] < val[s[i + 1]]:
+                total -= val[s[i]]
+            else:
+                total += val[s[i]]
+
+        return total + val[s[-1]]
+        
